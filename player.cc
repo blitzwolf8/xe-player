ae959757623fce015a403fb7a7df8be1adfb69ec
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2022-03-05 22:49:43 +0100

    app details

--- player.cc
+++ player.cc
@@ -1,11 +1,16 @@
 #include "mainwidget.h"
 
 #include <QApplication>
+#include <QCoreApplication>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    Xe::MainWidget w;
-    w.show();
-    return a.exec();
+    QCoreApplication::setApplicationName("xe-player");
+    QCoreApplication::setOrganizationName("xio");
+    QCoreApplication::setOrganizationDomain("xio.com");
+
+    QApplication app(argc, argv);
+    Xe::MainWidget xe_widget;
+    xe_widget.show();
+    return app.exec();
 }
